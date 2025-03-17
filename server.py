bd6f994aad9b1dc34bc2d75a001656d40f251ff7
A khan <awal.khan@live.co.uk>
2025-03-17 23:42:11 +0000

    updated html

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from http.client import responses
+
 from flask import Flask, render_template
 import datetime
 import requests
@@ -21,6 +23,15 @@ def guess(name):
     age_data= age_response.json()
     age=age_data["age"]
     return render_template("guess.html", person_name=name,gender=gender, age=age)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    #  fetch all this post
+    blog_url= "https://api.npoint.io/c790b4d5cab58020d391"
+    response= requests.get(blog_url)
+    all_posts =response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ =="__main__":
     app.run(debug=True)
